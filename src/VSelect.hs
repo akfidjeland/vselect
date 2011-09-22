d6330f9c37b09cba7955aaabf4f88defd0ab0813
Andreas Fidjeland <akf@doc.ic.ac.uk>
2011-09-22 17:19:07 +0100

    Factored selection widget into separate module

--- src/VSelect.hs
+++ src/VSelect.hs
@@ -1,97 +1,18 @@
 {-# OPTIONS_GHC -fno-warn-missing-signatures -fno-warn-unused-do-bind #-}
 module Main where
 
-import Control.Monad (when, forM, forM_)
-import Data.Char (isDigit, digitToInt)
+import Control.Monad (when)
 import Data.IORef
-import Data.List (intercalate)
-import Data.Maybe (fromJust)
 import Graphics.Vty
 import Graphics.Vty.Widgets.All
 import System.Exit (exitFailure)
 import System.Environment (getArgs)
-import System.IO (Handle, hPutStrLn, stdout, stderr, hGetContents)
+import System.IO (hPutStrLn, stdout, stderr, hGetContents)
 import System.Posix.Files (getFileStatus, isCharacterDevice)
 import System.Posix.IO
 import System.Posix.Types (Fd)
 
-
-data Entry = Entry {
-        index :: Maybe Char,
-        marked :: Bool,
-        value :: String
-    }
-
-
-instance Show Entry where
-    show (Entry n m ss) = (maybe ' ' id n) : " " ++ marker ++ ss
-        where
-            marker = if m then "* " else "  "
-
-toggle :: Entry -> Entry
-toggle (Entry i m v) = Entry i (not m) v
-
-
-type ListWidget = (Widget (List Entry FormattedText))
-
-
-toggleSelected :: ListWidget -> IO ()
-toggleSelected w = do
-    s <- getSelected w
-    case s of
-        Nothing -> return ()
-        (Just (i, (e, _))) -> do
-            removeFromList w i
-            let e' = toggle e
-            we <- plainText (show e')
-            insertIntoList w e' we i
-            setSelected w i
-
-
-allEntries :: ListWidget -> IO [Entry]
-allEntries w = do
-    n <- getListSize w
-    forM [0..n-1] $ \i -> return . fst . fromJust =<< getListItem w i
-
-
-{- The return is either
- - 1) just the currently selected item (if nothing has been marked); or
- - 2) all marked items
- -}
-selected :: ListWidget -> Entry -> IO String
-selected w e = do
-    entries <- allEntries w
-    let markedEntries = filter marked entries
-    if null markedEntries
-        then return $ value e
-        else return $ intercalate "\n" $ map value markedEntries
-
-
-listWidget :: [String] -> (String -> IO ()) -> IO ListWidget
-listWidget input exit = do
-    let attr = white `on` green
-        indices = (map Just ['1'..'9']) ++ repeat Nothing
-        entries = zipWith3 Entry indices (repeat False) input
-    w <- newList attr
-    forM_ entries $ \e ->
-        addToList w e =<< plainText (show e)
-
-    setFocusAttribute w $ attr
-
-    w `onItemActivated` \(ActivateItemEvent _ entry _) -> exit =<< selected w entry
-
-    w `onKeyPressed` \_ k _ -> do
-        case k of
-            (KASCII 'j') -> scrollDown w     >> return True
-            (KASCII 'k') -> scrollUp w       >> return True
-            (KASCII 'x') -> toggleSelected w >> return True
-            (KASCII c) -> do
-                when (isDigit c) $ do
-                    i <- getListItem w $ (digitToInt c) - 1
-                    maybe (return ()) (exit . value . fst) i
-                return False
-            _ -> return False
-    return w
+import SelectWidget (selectWidget)
 
 
 getInput :: Fd -> IO [String]
@@ -139,7 +60,7 @@ main = do
     output <- newIORef ""
     let exit str = writeIORef output str >> shutdownUi
 
-    wList <- listWidget input exit
+    wList <- selectWidget input exit
     ui <- centered wList
 
     fg <- newFocusGroup
